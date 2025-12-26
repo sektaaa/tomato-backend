091931310f2acd471ee7ec06c3c9a7c0fedf1b65
Ekta Sinha <youremail@example.com>
2025-12-26 21:35:13 +0530

    Secure MongoDB connection using enviornment variables

--- config/db.js
+++ config/db.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
- export const connectDB = async () => {
+//  export const connectDB = async () => {
     
-        await mongoose.connect('mongodb+srv://ektasinha:yashrajekta@cluster0.vfxfw4i.mongodb.net/food-del').then(() => console.log("DB connected"));
+//         await mongoose.connect('mongodb+srv://ektasinha:yashrajekta@cluster0.vfxfw4i.mongodb.net/food-del').then(() => console.log("DB connected"));
  
-    }  
+//     }  
+
+
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.log("DB connection error:", error);
+  }
+};
