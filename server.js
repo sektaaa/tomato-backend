aee31efff3b740938e3b3a6e49b7c2ccc5c6c963
Ekta Sinha <youremail@example.com>
2025-12-30 12:40:25 +0530

    Corrected ports for the backend

--- server.js
+++ server.js
@@ -13,31 +13,38 @@ import orderRouter from "./routes/orderRoute.js";
 
 //app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://stirring-torrone-00c1c1.netlify.app"
+    ],
+    credentials: true
+}));
+
 
 //db connection
 connectDB();
 
 //api endpoints
 app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter);
-app.use("/api/cart",cartRouter);
+app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("API working")
 })
 
-app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 })
 
 
-//mongodb+srv://ektasinha:yashrajekta@cluster0.vfxfw4i.mongodb.net/?appName=Cluster0
